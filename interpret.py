63144ba2953b46e25855d9d0b9c61d93cc76ed2f
Alexandru Rotaru <rotarualexandruandrei94@gmail.com>
2019-10-22 23:36:20 +0300

    Changed the test format a bit, refactored the code into classese (separated the traversing of the ast from the python interpreter).

--- interpret.py
+++ interpret.py
@@ -11,71 +11,13 @@ import threading
 import ptyprocess
 import select
 
-def indentLvl(codeLine):
-	tabCnt = 0
-	spcCnt = 0
-
-	for ch in codeLine:
-		tabCnt += (ch == '\t')
-		spcCnt += (ch == ' ')
-
-	if tabCnt:
-		return tabCnt
-
-	if spcCnt:
-		return spcCnt // 4
-
-def stripIndent(codeLine, indents=1):
-	# consider tab = 4 space
-	if codeLine[0] == '\t':
-		return codeLine[indents:] # indents tabs
-	if codeLine[0] == ' ':
-		return codeLine[indents * 4:]
-	return codeLine
-
-def runTest(tree, lines, interpreter, source = ""):
-	pprint(list(enumerate(lines)))
-	for node in ast.walk(tree):
-		if not isinstance(node, ast.Module) and \
-			not isinstance(node, ast.FunctionDef):
-			continue
-
-		if isinstance(node, ast.FunctionDef):
-			comments = lines[node.lineno - 2][4:]
-			values = eval(comments)
-			for idx, funcArg in enumerate(node.args.args):
-				code = f"{funcArg.arg} = {values[idx]}"
-				interpreter.interpret(code)
-				interpreter.getoutput()
-
-		for stmt in node.body:
-			code = astunparse.unparse(stmt)
-			interpreter.interpret(code)
-			if isinstance(stmt, ast.Assign):
-				for subExpr in ast.walk(stmt.value):
-					if subExpr is stmt.value:
-						continue
-					if isinstance(subExpr, ast.expr):
-						expr_val = astunparse.unparse(subExpr).strip()
-						interpreter.interpret(expr_val)
-						print(f"#{expr_val} = {interpreter.getoutput()}")
-
-				#for subExpr in stmt
-				stored = "%s" % stmt.targets[0].id
-				interpreter.interpret(stored)
-				print(f"#{stored} = {interpreter.getoutput()}")
-
-
-def runTests(interpreter):
-	with open("scenarios.py", "r") as f:
-		allText = f.read().split("#$#$")
-		for test in allText:
-			lines = test.split("\n")
-			tree = ast.parse(test)
-			runTest(tree, lines, interpreter, test)
-
 
 class Interpreter:
+	"""
+	Interpreter is an abstraction layer over a process of python in which we run
+	and evaluate all of the instructions/statements. We interact with it by
+	sending to the process the instruction to run and by getting the output.
+	"""
 	def __init__(self):
 		self.proc = ptyprocess.PtyProcessUnicode.spawn(['python'])
 		self.proc.setecho(False)
@@ -92,20 +34,65 @@ class Interpreter:
 			resp += self.proc.read()
 
 		# Cleaning out the >>> prompt from the interpreter
-		prompt = ">>>"
+		prompt = ">"
 		while resp.strip(" ").startswith(prompt):
 			resp = resp[len(prompt):].strip()
 		while resp.strip(" ").endswith(prompt):
 			resp = resp[:-len(prompt)].strip()
 		return resp
 
-pyInterpreter = Interpreter()
-pyInterpreter.getoutput() # get out the python notice
+class DebugFlow:
+	"""
+		DebugFlow contain the logic for ast traversal, detecting what object to
+		render and what subexpression to display.
+	"""
+	def __init__(self, interp = None):
+		self.interpreter = interp if interp else Interpreter()
+		self.interpreter.getoutput() # get out the python notice
 
-#while True:
-#	print(pyInterpreter.getOutput())
-#	pyInterpreter.interpret(input())
+	def flow(self, code):
+		# For now we take the whole code since we might get invalid code so we
+		# could try to discard lines to have correct code.
+		print(code)
+		tree = ast.parse(code)
+		for node in ast.walk(tree):
+			if not isinstance(node, ast.Module) and \
+				not isinstance(node, ast.FunctionDef):
+				continue
 
+			if isinstance(node, ast.FunctionDef):
+				comments = ast.get_docstring(node)
+				values = eval(comments)
+				for idx, funcArg in enumerate(node.args.args):
+					code = f"{funcArg.arg} = {values[idx]}"
+					self.interpreter.interpret(code)
+					self.interpreter.getoutput()
+
+			for stmt in node.body:
+				code = astunparse.unparse(stmt)
+				self.interpreter.interpret(code)
+				self.interpreter.getoutput()
+
+				if isinstance(stmt, ast.Assign):
+					for subExpr in ast.walk(stmt.value):
+						if subExpr is stmt.value:
+							continue
+						if isinstance(subExpr, ast.expr):
+							expr = astunparse.unparse(subExpr).strip()
+							self.interpreter.interpret(expr)
+							print(f"#{expr} = {self.interpreter.getoutput()}")
+					stored = "%s" % stmt.targets[0].id
+					self.interpreter.interpret(stored)
+					print(f"#{stored} = {self.interpreter.getoutput()}")
+
+
+
+def runTests(debug):
+	with open("scenarios.py", "r") as f:
+		allText = f.read().split("#$#$")
+		for test in allText[2:]:
+			debug.flow(test)
 
 if __name__ == "__main__":
-	runTests(pyInterpreter)
+	debug = DebugFlow()
+	runTests(debug)
--- scenarios.py
+++ scenarios.py
@@ -4,14 +4,14 @@ b = 3
 c = a * b + a ** b
 ###
 #$#$
-### 1, 2
 def sum(a, b):
+	"1, 2"
 	rez = a + b
 	return rez
 ###
 #$#$
-### [1, 2, 3, 4], 3
 def shift(lst, delta):
+	"[1, 2, 3, 4], 3"
 	for i in range(len(lst)):
 		lst[i] += delta
 	lst.append(delta)
